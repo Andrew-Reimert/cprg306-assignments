ccd33100d9f6bc821b7aafb25710767e9a3202c4
Andrew-Reimert <andrew.reimert@edu.sait.ca>
2024-01-30 19:11:01 -0700

    week 2 lab
    This is code that was made for the week lab

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <ul>
+      <li>Andrew Reimert</li>
+      <li><Link href="https://github.com/Andrew-Reimert">https://github.com/Andrew-Reimert</Link></li>
+    </ul>
+  );
+}
