7af4e236563c94bbc558f1f863db35f7fe734bda
Andrew-Reimert <andrew.reimert@edu.sait.ca>
2024-04-02 23:51:45 -0600

    Update week-10
    got rid of redundant function

--- app/week-10/shopping-list/page.js
+++ app/week-10/shopping-list/page.js
@@ -21,11 +21,6 @@ function removeEmojis(text){
   return text.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
 }
 
-async function loadItems(user) {
-  const items = await getItems(user.uid);
-  setItems(items);
-}
-
 
 export default function Page() {
   const [items, setItems] = useState([]);
